8d13515ab7263ac5c751bde0a80195a5d37834a1
Jerinji2016 <jerinji2016@gmail.com>
2023-02-04 21:19:49 +0530

    update api version log

--- src/app.service.ts
+++ src/app.service.ts
@@ -6,6 +6,8 @@ export class AppService {
   constructor(private configService: ConfigService) { }
 
   getBaseUrlCheck(): string {
-    return `Running on Version: ${this.configService.get('port')}`;
+    const version = this.configService.get('apiVersion');
+    const port = this.configService.get('port');
+    return `Running on Version: ${version} port: ${port}`;
   }
 }
--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,4 +1,5 @@
 export const config = () => ({
     port: Number(process.env.PORT),
     mongoUri: process.env.MONGO_URI,
+    apiVersion: process.env.VERSION
 });
